d815ceceee78a5d41190a22c517b82ea0feca0fc
Gabriel Souza <gabrie.ssouza@gmail.com>
2018-01-09 19:27:21 -0300

    Updated docs

--- src/modules/realties/routes/_apidocs.ts
+++ src/modules/realties/routes/_apidocs.ts
@@ -1,9 +1,38 @@
 /**
- * @api {get} /users Lista todos os usuários
- * @apiName UsersList
- * @apiGroup Users
+ * @api {get} /realties Lista todos os imóveis
+ * @apiName RealtiesList
+ * @apiGroup Realties
  * @apiVersion  1.0.0
- * @apiPermission Token
- * @apiHeader {String} Authorization Bearer token
+ */
+
+/**
+ * @api {get} /realties/:id Lista um imóveis
+ * @apiName RealtyList
+ * @apiParam {String} id Id do imóvel
+ * @apiGroup Realties
+ * @apiVersion  1.0.0
+ */
+
+/**
+ * @api {post} /realties Cria um imóveis
  *
+ * @apiParam{String}   code                  Codigo do imóvel.
+ * @apiParam{String}   name                  Nome do imóvel.
+ * @apiParam{String}   price                 Preço.
+ * @apiParam{Object}   address               Endereço.
+ * @apiParam{String}   address.cep           CEP.
+ * @apiParam{String}   address.publicPlace   Logradouro.
+ * @apiParam{String}   address.number        Número.
+ * @apiParam{String}   address.city          Cidade.
+ * @apiParam{String}   address.uf            Estado.
+ * @apiParam{String}   address.complement    Complemento.
+ * @apiParam{String}   quantBedrom           Quantidade de Quartos.
+ * @apiParam{String}   usefulArea            Area construida .
+ * @apiParam{String}   quantBathroom         Quantidade de Banheiros.
+ * @apiParam{String}   quantGarages          Quantidade de Garagens.
+ * @apiParam{String}   quantSuite            Quantidade de Suites.
+ *
+ * @apiName RealtyCreate
+ * @apiGroup Realties
+ * @apiVersion  1.0.0
  */
